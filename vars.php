1354695457a8b9df96e135937e0d486844d94078
muhammadfadhlillahrasyid <86339015+muhammadfadhlillahrasyid@users.noreply.github.com>
2021-06-23 11:52:07 +0700

    Add files via upload

---
+++ vars.php
@@ -0,0 +1,7 @@
+<?php
+$wa_01 ="https://api.whatsapp.com/send?text=Hai%20Bro,%20Boleh%20Tanya-Tanya%20Tarantula%20Apparel-nya%20?%20%3CMFR%3E&phone=6281260779669";
+$wa_02 ="#";
+$wa_tshrit="https://api.whatsapp.com/send?text=Hai%20Bro,%20Ada%20Promo%20Gak%20untuk%20Tarantula%20T-Shirtnya%20?%20%3CMFR%3E&phone=6281260779669";
+$wa_jacket ="https://api.whatsapp.com/send?text=Hai%20Bro,%20Gue%20Mau%20Dong%20Tarantula%20Jaket-nya%20?%20%3CMFR%3E&phone=6281260779669";
+$wa_sweater="https://api.whatsapp.com/send?text=Hai%20Bro,%20Boleh%20Tanya-Tanya%20Tarantula%20Apparel-nya%20?%20%3CMFR%3E&phone=6281260779669";
+?>

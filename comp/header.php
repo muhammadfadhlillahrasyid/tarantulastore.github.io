6ecc99ae033f45a547d09b80e6148f3da2bb83f4
muhammadfadhlillahrasyid <86339015+muhammadfadhlillahrasyid@users.noreply.github.com>
2021-06-23 11:49:16 +0700

    Add files via upload

---
+++ comp/header.php
@@ -0,0 +1,122 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Tarantula :: Official Webstore</title>
+        
+<!-- 
+
+Sentra Template
+
+https://templatemo.com/tm-518-sentra
+
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/light-box.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+        <link rel="shortcut icon" href="img/favicon.jpg" type="image/jpg">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+<body>
+
+
+
+        <header class="nav-down responsive-nav hidden-lg hidden-md">
+            <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!--/.navbar-header-->
+            <div id="main-nav" class="collapse navbar-collapse">
+                <nav>
+                    <ul class="nav navbar-nav">
+                        <li><a href="#top">Home</a></li>
+                        <li><a href="#allproduct">All Product</a></li>
+                        <li><a href="#tshirt">T-Shirt</a></li>
+                        <li><a href="#jacket">Jacket</a></li>
+                        <li><a href="#sweater">Sweater</a></li>
+                        <li><a href="#contact">Contact Us</a></li>
+                        <li><a href="#order">How To Order</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <div class="sidebar-navigation hidde-sm hidden-xs">
+            <div class="logo">
+                <!--<a href="#">Sen<em>tra</em></a>-->
+                <a href="#"><img class="u-max-full-width" src="img/logo-02.png"></a>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#top">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#allproduct">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            All Product
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tshirt">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            T-Shirt
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#jacket">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            Jacket
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#sweater">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            Sweater
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            Contact Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#order">
+                            <span class="rect"></span>
+                            <span class="circle"></span>
+                            How To Order
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <ul class="social-icons">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+
+            </ul>
+        </div>
